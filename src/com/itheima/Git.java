7eb446e96f029afb3cb53aa1c087565dfbfc70da
root-453972717 <w120653305@gmail.com>
2019-06-29 21:37:44 +0800

    协同开发第一次修改;

--- src/com/itheima/Git.java
+++ src/com/itheima/Git.java
@@ -7,11 +7,13 @@ public class Git {
         System.out.println("醒也无聊!..");
         System.out.println("醉也无聊..!");
 
+        //云想衣裳花想容,春风拂槛露华浓 ;
     }
 
     public void test(){
         int i = 10 ;
         i += 10 ;
+        System.out.println(i);
 
     }
 }
