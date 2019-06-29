8d80e82f967f380f8be14f81a222b64cee4c48ea
wang <lovezmdezj@qq.com>
2019-06-29 20:18:39 +0800

    本地工程添加到Git

---
+++ src/com/itheima/Git.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class Git {
+
+    public static void main(String[] args) {
+
+        System.out.println("醒也无聊!..");
+        System.out.println("醉也无聊..!");
+
+    }
+}
